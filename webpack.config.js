e0d35a9775945f6e8e67c3ea195628f7db8d99b8
Boyer-Inc <64108961+SykoNaught@users.noreply.github.com>
2021-06-24 17:55:01 -0500

    File Dump

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+ 
+module.exports = {
+    entry: path.resolve(__dirname, './src/index.js'),
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    }
+};
